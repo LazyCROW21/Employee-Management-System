4823dfa49e2b45268d4e741a84ed4eace6fd0c35
LazyCROW <hardikkardam21@gmail.com>
2022-01-09 02:18:32 +0530

    sidenav added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
-  <p>Hello</p>
+  <NavBar/>
 </template>
 
 <script>
 // @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
+import NavBar from '@/components/NavBar.vue'
 
 export default {
   name: 'Home',
   components: {
-    // HelloWorld
+    NavBar
   }
 }
 </script>
